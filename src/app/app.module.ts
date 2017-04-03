ab6a953dfcc53d6cff1e43d9c073b95aab1e0212
lucasleclercq <lucas@belinks.fr>
2017-04-03 22:28:02 +0200

    dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,37 @@
-import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule }   from '@angular/forms';
+import { NgModule }       from '@angular/core';
+import { BrowserModule }  from '@angular/platform-browser';
+import { FormsModule }    from '@angular/forms';
+import { RouterModule }   from '@angular/router';
 
-import { AppComponent }  from './app.component';
+import { AppComponent }        from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
-import { HeroesComponent } from './heroes.component';
-import { HeroService }         from './hero.service';
-
+import { HeroesComponent }     from './heroes.component';
+import { HeroService } from './hero.service';
+import { DashboardComponent } from "./dashboard.component";
 
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: 'heroes',
+        component: HeroesComponent
+      },
+      {
+        path: 'dashboard',
+        component: DashboardComponent
+      },
+      {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full'
+      },
+    ])
   ],
   declarations: [
     AppComponent,
+    DashboardComponent,
     HeroDetailComponent,
     HeroesComponent
   ],
@@ -23,4 +40,5 @@ import { HeroService }         from './hero.service';
   ],
   bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {
+}
